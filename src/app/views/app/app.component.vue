09da09f649c4e6e3675880c86b6c02017b862d85
Sean Wood <woodywoodsta@gmail.com>
2017-12-16 17:05:02 +0000

    Setup style and grid

--- src/app/views/app/app.component.vue
+++ src/app/views/app/app.component.vue
@@ -1,5 +1,14 @@
 <template>
-<h1>Hello World</h1>
+  <section>
+    <div class="container">
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti debitis dignissimos expedita alias. Eveniet voluptatem expedita optio tenetur ut. Facere nostrum ab similique voluptas id odit nisi distinctio sapiente iure.</p>
+    </div>
+    <div class="container">
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti debitis dignissimos expedita alias. Eveniet voluptatem expedita optio tenetur ut. Facere nostrum ab similique voluptas id odit nisi distinctio sapiente iure.</p>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -16,6 +25,21 @@ export const AppComponentAsync = new Promise((resolve) => {
 </script>
 
 <style lang="stylus">
-// Global Stylesheet
-@require '../../assets/styles/index';
+  // Global Stylesheet
+  @require '../../assets/styles/index';
+
+  section {
+    grid()
+    +below(rem-calc(breakpoints[md])) {
+      grid('column');
+    }
+
+    .container {
+      cell(1, 2);
+
+      background: $light-weak-alpha;
+      border-radius: 2px;
+      padding: $padding-normal;
+    }
+  }
 </style>
