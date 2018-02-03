639ffc5c23abe22f64ac74a7ddd1e9265a214722
Sean Wood <woodywoodsta@gmail.com>
2018-02-03 21:46:37 +0200

    Add mobile side pane button bar

---
+++ src/views/viewer/components/mobile-side-pane-toggle.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="mobile-side-pane-toggle" :class="{ active }">
+    <div class="button-container">
+      <template v-if="!active">
+        <fa-icon class="button-icon bars-icon" icon="bars"></fa-icon>
+        <span>Menu</span>
+      </template>
+      <fa-icon v-if="active" class="button-icon arrow-left-icon" icon="arrow-left"></fa-icon>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'mobile-side-pane-toggle',
+
+  props: {
+    active: Boolean,
+  },
+};
+</script>
+
+<style lang="scss">
+  .mobile-side-pane-toggle {
+    @include flexbox;
+    @include flex-direction(row);
+    @include justify-content(start);
+
+    > .button-container {
+      @include flexbox;
+      @include flex-direction(row);
+      @include align-items(center);
+
+      > .button-icon {
+        height: 18px;
+        width: 18px;
+      }
+
+      > .bars-icon {
+        @include mr(sm);
+      }
+    }
+
+    &.active {
+      @include justify-content(flex-end);
+    }
+  }
+</style>
--- src/views/viewer/viewer-view.component.vue
+++ src/views/viewer/viewer-view.component.vue
@@ -1,31 +1,42 @@
 <template>
   <section class="viewer-view" :class="{ sidePaneOpen }">
-    <div class="side-pane-wrapper">
-      <side-pane
-        :views="views"
-        :active-view="currentView"
-        @menu-item-click="_onMenuItemClick">
-      </side-pane>
+    <mobile-side-pane-toggle class="mobile-side-pane-toggle-component"
+      :class="{ active: sidePaneOpen }"
+      :active="sidePaneOpen"
+      @click.native="_onMobileSidePaneToggleClick">
+    </mobile-side-pane-toggle>
+
+    <!-- Horizontal Client and Side Panes -->
+    <div class="pane-area">
+      <div class="side-pane-wrapper">
+        <side-pane
+          :views="views"
+          :active-view="currentView"
+          @menu-item-click="_onMenuItemClick">
+        </side-pane>
+      </div>
+      <system-window-section class="viewer-pane">
+        <router-view></router-view>
+      </system-window-section>
     </div>
-    <system-window-section class="viewer-pane">
-      <router-view></router-view>
-    </system-window-section>
   </section>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions, mapMutations } from 'vuex';
 
-import { VIEWS, ACTIONS } from '../../constants/navigation';
+import * as navigationConsts from '../../constants/navigation';
+import * as systemConsts from '../../constants/system';
 
 import SidePaneComponent from './components/side-pane.comonent.vue';
+import MobileSidePaneToggleComponent from './components/mobile-side-pane-toggle.vue';
 
 export default {
   name: 'viewer-view',
 
   data() {
     return {
-      views: Object.values(VIEWS.VIEWER.children),
+      views: Object.values(navigationConsts.VIEWS.VIEWER.children),
     };
   },
   computed: {
@@ -34,15 +45,21 @@ export default {
   },
 
   methods: {
-    ...mapActions('navigation', [ACTIONS.NAVIGATE]),
+    ...mapActions('navigation', [navigationConsts.ACTIONS.NAVIGATE]),
+    ...mapMutations('system', [systemConsts.MUTATIONS.SET_SIDE_PANE_OPEN]),
 
     _onMenuItemClick({ view }) {
-      this[ACTIONS.NAVIGATE](view.name);
+      this[navigationConsts.ACTIONS.NAVIGATE](view.name);
+    },
+
+    _onMobileSidePaneToggleClick() {
+      this[systemConsts.MUTATIONS.SET_SIDE_PANE_OPEN](!this.sidePaneOpen);
     },
   },
 
   components: {
     sidePane: SidePaneComponent,
+    mobileSidePaneToggle: MobileSidePaneToggleComponent,
   },
 };
 </script>
@@ -53,51 +70,74 @@ export default {
   .viewer-view {
     @include view(contained);
     @include flexbox;
-    @include flex-direction(row);
+    @include flex-direction(column);
 
-    > .side-pane-wrapper {
-      min-width: $side-pane-width;
+    > .mobile-side-pane-toggle-component {
+      @include mt(md);
+      @include mx(md);
 
-      transition: all $transition;
-      transition-property: opacity;
-
-      @include media-breakpoint-down(md) {
-        min-width: $side-pane-width-md;
-      }
-
-      @include media-breakpoint-down(sm) {
-        min-width: $side-pane-width;
-        opacity: 0;
+      @include media-breakpoint-up(md) {
+        display: none;
       }
     }
 
-    > .viewer-pane {
-      box-sizing: border-box;
-      overflow-y: scroll;
-
+    > .pane-area {
+      @include flexbox;
+      @include flex-direction(row);
       @include flex;
-      @include m(md);
-      @include ml(none);
 
-      transition: all $transition;
-      transition-property: transform;
+      > .side-pane-wrapper {
+        position: relative;
+        min-width: $side-pane-width;
 
-      @include media-breakpoint-up(md) {
-        margin-top: calc(#{-$window-title-bar-height * 2} + #{get-size(md)});
+        transition: all $transition;
+        transition-property: opacity;
+
+        @include media-breakpoint-down(md) {
+          min-width: $side-pane-width-md;
+        }
+
+        @include media-breakpoint-down(sm) {
+          min-width: $side-pane-width;
+          opacity: 0;
+          pointer-events: none;
+        }
       }
 
-      @include media-breakpoint-down(sm) {
-        margin-left: $translate-x-width;
+      > .viewer-pane {
+        box-sizing: border-box;
+        overflow-y: scroll;
+
+        @include flex;
+        @include m(md);
+        @include ml(none);
+
+        transition: all $transition;
+        transition-property: transform;
+
+        @include media-breakpoint-up(md) {
+          margin-top: calc(#{-$window-title-bar-height * 2} + #{get-size(md)});
+        }
+
+        @include media-breakpoint-down(sm) {
+          margin-left: $translate-x-width;
+        }
       }
     }
 
     &.sidePaneOpen {
-      > .side-pane-wrapper {
-        opacity: 1;
-      }
+      > .pane-area {
+        > .side-pane-wrapper {
+          opacity: 1;
+        }
 
-      > .viewer-pane {
-        transform: translateX(calc(-1 * #{$translate-x-width}));
+        > .viewer-pane {
+          transform: translateX(calc(-1 * #{$translate-x-width}));
+        }
+
+        > .mobile-side-pane-toggle-component {
+          transform: translateX(calc((-1 * #{$translate-x-width}) + #{get-size(md)}))
+        }
       }
     }
   }
